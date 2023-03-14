c81930033874788018174664e5cd612dac866ff2
dongminsss <tyg564@naver.com>
2023-03-14 18:26:43 +0900

    동민 프로젝트관리 프로젝트 수정 빼고 다 구현

--- src/main/java/com/choongang/scheduleproject/controller/AdminRestController.java
+++ src/main/java/com/choongang/scheduleproject/controller/AdminRestController.java
@@ -7,12 +7,19 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.choongang.scheduleproject.command.CheckVO;
+import com.choongang.scheduleproject.command.ProjectCheckVO;
+import com.choongang.scheduleproject.command.ProjectDetailMemberVO;
+import com.choongang.scheduleproject.command.ProjectDetailVO;
 import com.choongang.scheduleproject.command.UserVO;
 import com.choongang.scheduleproject.service.AdminService;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 @RestController
 public class AdminRestController {
@@ -30,6 +37,36 @@ public class AdminRestController {
 		}
 		return "등록 실패했습니다.";
 	}
-	
+	@PostMapping("/projectCheckRegist")
+	public String manageProject(@RequestBody ArrayList<ProjectCheckVO> list) { //JSON데이터를 리스트 형태로 받음
+		int updateResult = adminService.updateProjectList(list);
+		int deleteResult = adminService.deleteProjectList(list);
+		if(updateResult == 1) {
+			return "등록 성공했습니다.";
+		}
+		return "등록 실패했습니다.";
+	}
+	@PostMapping("/getProjectDetail")
+	public String getProjectDetail(@RequestBody ProjectDetailVO vo) { //
+		ProjectDetailVO ProjectDetailData = adminService.getProjectDetail(vo);
+		ArrayList<ProjectDetailMemberVO> ProjectDetailMember = adminService.getProjectDetailMember(vo);
+		JsonArray team = new JsonArray(); //팀원 
+		JsonArray observer = new JsonArray(); //옵저버
+		JsonObject ProjectDetail = new JsonObject(); //넘겨줄 데이터 Gson의 json으로 파싱
+		ProjectDetail.addProperty("projectName", ProjectDetailData.getPj_name()); //프로젝트 이름 문자열로 담아줌
+		ProjectDetail.addProperty("projectDescription", ProjectDetailData.getPj_description()); //프로젝트 설명 문자열로 담아줌
+		//프로젝트의 옵저버와 팀원을 구분해서 담아줌
+		for(ProjectDetailMemberVO pdmVO : ProjectDetailMember ) {
+			if(pdmVO.is_observer() == true) {
+				observer.add(pdmVO.getUser_name());
+			} else {
+				team.add(pdmVO.getUser_name());				
+			}
+		}
+		ProjectDetail.add("team", team); //배열로 담아줌
+		ProjectDetail.add("observer", observer); 
+		
+		return ProjectDetail.toString(); //json 스트링으로 반환
+	}
 	
 }
--- src/main/java/com/choongang/scheduleproject/service/AdminService.java
+++ src/main/java/com/choongang/scheduleproject/service/AdminService.java
@@ -3,6 +3,9 @@ package com.choongang.scheduleproject.service;
 import java.util.ArrayList;
 
 import com.choongang.scheduleproject.command.CheckVO;
+import com.choongang.scheduleproject.command.ProjectCheckVO;
+import com.choongang.scheduleproject.command.ProjectDetailMemberVO;
+import com.choongang.scheduleproject.command.ProjectDetailVO;
 import com.choongang.scheduleproject.command.ProjectVO;
 import com.choongang.scheduleproject.command.UserVO;
 import com.choongang.scheduleproject.util.Criteria;
@@ -15,4 +18,8 @@ public interface AdminService {
 	public int deleteMember(ArrayList<CheckVO> list);//회원 삭제
 	public int getProjectCount(Criteria criteria); //프로젝트 검색결과 총갯수
 	public ArrayList<ProjectVO> getProjectList(Criteria criteria);// 프로젝트 목록 출력
+	public int updateProjectList(ArrayList<ProjectCheckVO> list); //프로젝트 관리 체크박스 업데이트
+	public int deleteProjectList(ArrayList<ProjectCheckVO> list);//프로젝트 관리 삭제 기능
+	public ProjectDetailVO getProjectDetail(ProjectDetailVO vo); //프로젝트 상세내용 받아오는 것
+	public ArrayList<ProjectDetailMemberVO> getProjectDetailMember(ProjectDetailVO vo);//프로젝트에저장된 pj_num을 이용해서 해당 프로젝트 인원 가져오기
 }
