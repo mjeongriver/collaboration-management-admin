d8ad633552b51e865b2449d6a147534b99124dc8
권도은 <kounduk9119t@naver.com>
2023-03-17 14:17:49 +0900

    doeun commit

--- src/main/java/com/choongang/scheduleproject/controller/HomeController.java
+++ src/main/java/com/choongang/scheduleproject/controller/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,21 +19,21 @@ import com.choongang.scheduleproject.util.PageVO;
 @Controller
 @RequestMapping("/admin")
 public class HomeController {
-	
+
 	@Autowired //dashBoard
 	AdminService adminService;
-	
+
 	@Autowired //공지사항
 	@Qualifier("adminNoticeService")
 	AdminNoticeService adminNoticeService;
-	
-	
+
+
 	@GetMapping("/")
 	public String index() {
 		return "admin/adminNoticeLogin";
 	}
-	
-	@GetMapping("/adminDashboard")
+
+	@GetMapping("/admin-dashboard")
 	public String adminDashboard(Model model , Criteria criteria) {
 	    int totalMember = adminService.getMemberCount(criteria); // 회원 총 갯수
 	    int totalProject = adminService.getProjectCount(criteria); // 프로젝트 총 갯수
@@ -47,44 +46,45 @@ public class HomeController {
 	    model.addAttribute("projectList", adminService.getProjectList(criteria)); // 프로젝트 목록 데이터
 	    model.addAttribute("projectPageVO", projectPageVO); // 프로젝트 목록 페이지 정보
 
-	    return "admin/adminDashboard";
+	    return "admin/admin-dashboard";
 	}
-	
+
 	//관리자 공지사항 게시판
-	@GetMapping("/noticeTableList")
-	public String adminNoticeTableList(Criteria cri, Model model) {
-		
+	@GetMapping("/notice-tablelist")
+	public String noticeTableList(Criteria cri, Model model) {
+
 		int total = adminNoticeService.getCount(cri);
 		model.addAttribute("AdminNoticeList", adminNoticeService.getList(cri)); //페이지에 넘길 데이터
-		
+
 		PageVO pageVO = new PageVO(cri, total); //페이징에 사용
 		System.out.println(pageVO.toString());
 		model.addAttribute("pageVO", pageVO);
-		
-		return "admin/noticeTableList";
+
+		return "admin/notice-tablelist";
 	}
-	
+
 	//위의 noticeTableList를 상세 조회하는 컨트롤러
-	@GetMapping("/noticeContent")
+	@GetMapping("/notice-content")
 	public String noticeContent(@RequestParam int notice_num, Model model) {
-			
+
 		//클릭한 글 번호에 대한 내용을 조회
 		AdminNoticeListVO adminNoticeListVO = adminNoticeService.getContent(notice_num);
 		model.addAttribute("adminNoticeListVO", adminNoticeListVO);
-			
-			return "admin/noticeContent";
+
+			return "admin/notice-content";
 		}
-	
-	@PostMapping(value="/deleteForm")
+
+	//삭제 기능
+	@PostMapping(value="/deleteform")
 	public String deleteNotice(@RequestParam("notice_num") int notice_num, RedirectAttributes ra) {
-		
+
 		int result = adminNoticeService.deleteNotice(notice_num);
 		String msg = result == 1 ? "삭제되었습니다" : "삭제에 실패했습니다";
 		ra.addFlashAttribute("msg", msg);
-		
-		return "redirect:/admin/noticeTableList";
+
+		return "redirect:/admin/notice-tablelist";
 	}
 
-	
+
 
 }
--- src/main/java/com/choongang/scheduleproject/util/PageVO.java
+++ src/main/java/com/choongang/scheduleproject/util/PageVO.java
@@ -6,52 +6,51 @@ import lombok.Data;
 
 @Data
 public class PageVO {
-	
+
 	private int start;
 	private int end;
 	private boolean prev;
 	private boolean next;
-	
+
 	private int page;
 	private int amount;
 	private int total;
 	private int realEnd;
-	
+
 	private Criteria criteria;
 	private ArrayList<Integer> pageList; //화면에 반복시킬
-	
-	private int pageCount = 10; //페이지네이션 개수
-	
-	
-	
+
+	public static final int PAGE_COUNT = 10; //페이지네이션 개수
+
+
 	public PageVO(Criteria criteria, int total) {
 		this.page = criteria.getPage();
 		this.amount = criteria.getAmount();
 		this.total = total;
 		this.criteria = criteria;
-		
+
 		//끝 페이지 번호
-		this.end = (int)Math.ceil(this.page/(double)pageCount) * pageCount;
-		this.start=this.end - pageCount + 1;
-		
+		this.end = (int)Math.ceil(this.page/(double)PAGE_COUNT) * PAGE_COUNT;
+		this.start=this.end - PAGE_COUNT + 1;
+
 		//실제 끝번호
-		
+
 		this.realEnd = (int)Math.ceil(this.total/(double)this.amount);
 		this.end = this.end > this.realEnd ? this.realEnd:this.end;
-		
+
 		//이전버튼 활성화
 		//start 1, 11
 		this.prev = this.start > 1;
-		
+
 		//다음버튼 활성화
 		this.next = this.realEnd > this.end;
-		
+
 		//화면에서 반복시킬 페이지 데이터
 		this.pageList = new ArrayList<>();
 		for(int i = this.start; i<=this.end; i++) {
 			pageList.add(i);
 		}
-		
+
 	}
 
 }
